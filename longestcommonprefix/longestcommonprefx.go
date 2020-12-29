e6ba698974aa963420feec13351eab9d44b67869
Noor <noormahammad@users.noreply.github.com>
2020-12-28 21:00:57 -0500

    longest common prefix problem solved

--- longestcommonprefix/longestcommonprefx.go
+++ longestcommonprefix/longestcommonprefx.go
@@ -1,13 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	input := []string{"flower", "flow", "flight"}
+
 	fmt.Println(longestCommonPrefix(input));
+	fmt.Println(alternateSolution(input));
 }
 
+//*************************************** Solution 1 ***************************************//
 func longestCommonPrefix(strs []string) string {    
+	if(len(strs) == 0) {
+			return ""
+	}    
+	prefix := strs[0]        
+	
+	for i:=1; i<len(strs); i++ {
+			for(strings.Index(strs[i], prefix) != 0) {
+					prefix = prefix[0:len(prefix)-1]  
+					if(prefix == "") {
+							return ""
+					}
+			}               
+	}
+	return prefix
+}
+
+//*************************************** Solution 2 ***************************************//
+
+func alternateSolution(strs []string) string {    
 	prefix := ""
 	
 	if(len(strs) != 0) {
@@ -35,3 +60,5 @@ func getCommonPrefix(prev string, current string) string {
 	}
 	return prev[0:counter]
 }
+
+//********************************************************************************************//
