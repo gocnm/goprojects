4f4c6eb9883e4e426c2169547568c5844ed6ad36
Noor <noormahammad@users.noreply.github.com>
2020-12-31 20:58:38 -0500

    Uploading pictures concurrently

--- concurrency/concurrency.go
+++ concurrency/concurrency.go
@@ -4,9 +4,36 @@ import(
 "fmt"
 "time"
 "sync"
+"math/rand"
+"strconv"
 )
 
 func main() {
+	//runfunctionsconcurrently()
+	uploadPicturesConcurrently()
+}
+
+func uploadPicturesConcurrently() {
+	var wg sync.WaitGroup
+	for i:=1; i<100; i++ {		
+		wg.Add(1)
+		go func(pic string) {			
+			uploadPicture("Pic " + pic + ".jpg")
+			wg.Done()
+		}(strconv.Itoa(i))
+	}
+	wg.Wait()
+}
+
+func uploadPicture(path string) {
+	s1 := rand.NewSource(time.Now().UnixNano())
+	r1 := rand.New(s1)
+	processingTime := 200 * r1.Intn(15)
+	time.Sleep(time.Millisecond * time.Duration(processingTime))	
+	fmt.Println(path, "uploaded in ", strconv.Itoa(processingTime), " milli seconds" )
+}
+
+func runfunctionsconcurrently() {
 	var wg sync.WaitGroup
 	wg.Add(1)
 	go func() {
@@ -28,9 +55,11 @@ func main() {
 
 	wg.Wait()
 }
+
 func count(thing string) {
 	for i:=0; i < 5;i++ {
 		fmt.Println(i, thing)
 		time.Sleep(time.Millisecond * 500)
 	}		
 }
+
